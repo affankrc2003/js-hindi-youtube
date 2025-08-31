572779a406152f700ac0cce574bdfab2fc6f3521
affankrc2003 <affankrc2003@gmail.com>
2025-08-31 08:40:27 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail="affan@google.com"
+var accountPassword="12345"
+accountCity = "baleswar"
+let accountstate;
+
+// accountId=2//   not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword ="788789"
+accountCity ="soro"
+
+
+console.log(accountId);
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+
+*/
+
+console.table ([accountId,accountEmail,accountPassword,accountCity,accountstate])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node js not browser 
+
+console.log(3+
+    3) ; 
+    console.log("Hitesh")
+// code redeaibility should be high //
+
+let name="affan"
+let age= 20
+let isLoggedIn= false 
+let state;
+
+// number =>2 to power 53 
+// bigInt 
+// string =>""
+// boolean => true/false
+// null => stand alone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "hitesh");
+
+console.log(typeof undefined); // undefined output because it is a type
+console.log(typeof null); // it is a object
